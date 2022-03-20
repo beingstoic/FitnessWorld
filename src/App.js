33ea14c100669b522e3f9f67436050d5f9e5f891
Rishabh Goyal <rishagoy@amdocs.com>
2022-03-20 11:53:10 +0530

    Modularized the code and implemented the footer part

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import React from 'react'
-import HeaderComponent from './HeaderComponent';
-import About from './About';
-import Homepage from './Homepage';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Contact from './Contact';
+import React from "react";
+import HeaderComponent from "./Components/HeaderComponent";
+import About from "./Pages/About";
+import Homepage from "./Pages/Homepage";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Contact from "./Pages/Contact";
+import Footer from "./Components/Footer";
+
 export default function App() {
   return (
     <BrowserRouter>
-    <div>
-    <HeaderComponent />
-    <Routes>
-          <Route path="/about" element={<About/>}/>
-          <Route path="/contact-us" element={<Contact/>}/>
-          <Route path="" element={<Homepage/> }/>
-            
+      <div>
+        <HeaderComponent />
+        <Routes>
+          <Route path="/about" element={<About />} />
+          <Route path="/contact-us" element={<Contact />} />
+          <Route path="" element={<Homepage />} />
         </Routes>
-    <div className="footer">
-        <img src="/assets/bg.png" className="footer-logo" alt="" />
+        <Footer />
       </div>
-    </div>
     </BrowserRouter>
-  )
+  );
 }
